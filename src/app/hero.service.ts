75978a185d9cd9565f68517d437126a6546fca3a
Peter McCready <peter.mccready@experian.com>
2020-01-13 09:57:33 +0000

    use observable and of to return heroes

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 
@@ -9,7 +10,8 @@ export class HeroService {
 
   constructor() { }
 
-  getHeroes(): Hero[] {
-    return HEROES;
+  getHeroes(): Observable<Hero[]> {
+    // of(HEROES) returns an Observable<Hero[]> that emits a single value, the array of mock heroes.
+    return of(HEROES);
   }
 }

1d079ce4c161228cbebc2cca5e69ef06a49fb597
Peter McCready <petermccready2@gmail.com>
2020-01-13 19:45:21 +0000

    delete hero

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -57,13 +57,23 @@ export class HeroService {
     return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
       tap(_ => this.log(`updated hero id=${hero.id}`)),
       catchError(this.handleError<any>('updateHero'))
-    )
+    );
   }
 
   addHero(hero: Hero): Observable<Hero> {
     return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
       tap((newHero: Hero) => { this.log(`added hero w/ id=${newHero.id}`)}),
       catchError(this.handleError<Hero>('addHero'))
-    )
+    );
+  }
+
+  deleteHero(hero: Hero | number): Observable<Hero> {
+    const id = typeof hero === 'number' ? hero : hero.id;
+    const url = `${this.heroesUrl}/${id}`;
+
+    return this.http.delete<Hero>(url, this.httpOptions).pipe(
+      tap(_ => this.log(`deleted hero id=${id}`)),
+      catchError(this.handleError<any>('deleteHero'))
+    );
   }
 }
